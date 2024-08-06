2bc29276a1855330f1a67c75f37eecf4e89a783b
sdsdsa <124785062+Seunghyun-FE@users.noreply.github.com>
2024-08-06 17:58:01 +0900

    errorpage

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Container } from "./ErrorPage.styles";
+import img from "../../assets/images.jpg";
+import { ButtonFill } from "../../styles/styles";
+import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <div className="error__img">
+        <img src={img} alt="pageNotFound"></img>
+      </div>
+      <div className="error__text">
+        <h1>404</h1>
+        <div>error 발생</div>
+        <ButtonFill onClick={() => navigate("/")}>
+          <span>to MainPage</span>
+        </ButtonFill>
+      </div>
+    </Container>
+  );
 };
 
 export default ErrorPage;
